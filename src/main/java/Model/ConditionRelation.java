2913f8a97fc2a75aa83383f1d466604da1eecebd
Michael Kowollik <Michael.Kowollik@smail.inf.h-brs.de>
2020-07-22 13:41:59 +0200

    Added condition relation

---
+++ src/main/java/Model/ConditionRelation.java
@@ -0,0 +1,48 @@
+/*
+@author mkowol2s
+ */
+
+package Model;
+
+import java.util.List;
+
+public class ConditionRelation implements UMLComponent{
+    private List<UMLComponent> connectedElements;
+
+    public List<UMLComponent> getConnectedElements() {
+        return connectedElements;
+    }
+
+    public void setConnectedElements(List<UMLComponent> connectedElements) {
+        this.connectedElements = connectedElements;
+    }
+
+    public void addConnectedElements(UMLComponent component) {
+        this.connectedElements.add(component);
+    }
+
+    public void removeConnectedElements(UMLComponent component) {
+        this.connectedElements.remove(component);
+    }
+
+    public Boolean isConnectedToNote(){
+        if(connectedElements.size()==1){
+            if(connectedElements.get(0) instanceof Note){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isConnectable() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "ConditionRelation{" +
+                "connectedElements=" + connectedElements +
+                '}';
+    }
+}
