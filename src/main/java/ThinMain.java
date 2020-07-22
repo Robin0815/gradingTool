487aaeb60fda4672696f5b9ff19e3a7b43bc2442
Robin0815 <32617832+Robin0815@users.noreply.github.com>
2020-07-22 16:58:32 +0200

    Main angepasst

--- src/main/java/ThinMain.java
+++ src/main/java/ThinMain.java
@@ -12,14 +12,14 @@ public class ThinMain {
         String file = "All-Elements.uxf";
         Parser a = new Parser();
         List<UMLComponent> l = a.parseFile(file);
-        //System.out.println(l.toString());
-        for (int i = 0; i< l.size();i++){
+        System.out.println(l.toString());
+        /*for (int i = 0; i< l.size();i++){
             UMLComponent tmp = l.get(i);
             if(tmp instanceof Use){
                 Use tmp2 = (Use) tmp;
                 System.out.println(tmp2.getStart());
             }
-        }
+        }*/
         /*for(int i = 0; i<l.size();i++){
             UMLComponent com = l.get(i);
             if(com instanceof Class){
