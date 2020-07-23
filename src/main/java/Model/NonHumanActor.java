b6bc6c30933e5a18a838bb35c12829dd7349061e
Michael Kowollik <Michael.Kowollik@smail.inf.h-brs.de>
2020-07-23 11:08:50 +0200

    Added NonHumanActor

---
+++ src/main/java/Model/NonHumanActor.java
@@ -0,0 +1,33 @@
+package Model;
+
+import Control.Visitor.Visitor;
+
+public class NonHumanActor implements UMLComponent{
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean isConnectable() {
+        return true;
+    }
+
+    @Override
+    public void accept(Visitor visitor) {
+
+    }
+
+    @Override
+    public String toString() {
+        return "NonHumanActor{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
