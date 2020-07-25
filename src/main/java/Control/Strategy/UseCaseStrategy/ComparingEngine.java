f2c09e75ed8cb789adb2bfe4f46ae916de971831
Michael Kowollik <Michael.Kowollik@smail.inf.h-brs.de>
2020-07-25 17:05:30 +0200

    Added some math3 functions

--- src/main/java/Control/Strategy/UseCaseStrategy/ComparingEngine.java
+++ src/main/java/Control/Strategy/UseCaseStrategy/ComparingEngine.java
@@ -1,7 +1,7 @@
 package Control.Strategy.UseCaseStrategy;
+import org.apache.commons.math3.stat.inference.TTest;
 
 public class ComparingEngine {
     public static void compareSolutions(Solution tutorsolution,Solution studentsolution){
-
     }
 }
