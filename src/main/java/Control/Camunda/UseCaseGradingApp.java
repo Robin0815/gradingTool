eeba441f6a0f3e948a038b8429336f76640f8011
Michael Kowollik <Michael.Kowollik@smail.inf.h-brs.de>
2020-08-04 12:09:13 +0200

    Changed project structure to accommodate camunda process

---
+++ src/main/java/Control/Camunda/UseCaseGradingApp.java
@@ -0,0 +1,10 @@
+package Control.Camunda;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Grading Tool")
+public class UseCaseGradingApp extends ServletProcessApplication {
+
+
+}
