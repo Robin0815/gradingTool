dacbd39ed0380cf62443a24e9fc082680833e96a
Michael Kowollik <Michael.Kowollik@smail.inf.h-brs.de>
2020-08-06 15:30:26 +0200

    Removed useless akoakko comments

--- src/main/java/Control/Camunda/UseCaseDelegate.java
+++ src/main/java/Control/Camunda/UseCaseDelegate.java
@@ -1,7 +1,6 @@
 package Control.Camunda;
 
 import Control.Parser.Parser;
-import Control.Strategy.Strategy;
 import Control.Strategy.UseCaseStrategy.Control.UseCaseStrategy;
 import Model.UMLComponent;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
@@ -10,22 +9,11 @@ import org.camunda.bpm.engine.delegate.JavaDelegate;
 import javax.inject.Named;
 import java.io.File;
 import java.util.List;
-import java.util.logging.Logger;
 
 @Named("UCmain")
 public class UseCaseDelegate implements JavaDelegate {
-    private final Logger LOGGER = Logger.getLogger(UseCaseDelegate.class.getName());
 
     public void execute(DelegateExecution execution) throws Exception {
-
-        LOGGER.info("\n\n  ... LoggerDelegate invoked by "
-                + "processDefinitionId=" + execution.getProcessDefinitionId()
-                + ", activtyId=" + execution.getCurrentActivityId()
-                + ", activtyName='" + execution.getCurrentActivityName() + "'"
-                + ", processInstanceId=" + execution.getProcessInstanceId()
-                + ", businessKey=" + execution.getProcessBusinessKey()
-                + ", executionId=" + execution.getId()
-                + " \n\n");
 
         String homeDir = System.getProperty("user.home");
         homeDir = homeDir.replace("\\", "\\\\");
@@ -48,17 +36,5 @@ public class UseCaseDelegate implements JavaDelegate {
         }
 
         execution.setVariable("Report", strategy.getReport());
-        /*String file = homeDir + "\\Documents\\Korrektur\\GoodUseCase.uxf";
-        File testFile = new File(file);
-        Parser a = new Parser();
-        List<UMLComponent> l = a.parseFile(testFile);
-        Strategy strategy = new UseCaseStrategy(0.2,0.1,0.4,2);
-        strategy.analyzeUML(l);
-        file = homeDir + "\\Documents\\Korrektur\\GoodUseCase.uxf";
-        testFile = new File(file);
-        l = a.parseFile(testFile);
-        strategy.analyzeUML(l);
-        */
-
     }
 }
