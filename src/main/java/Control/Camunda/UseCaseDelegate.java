b8d0a019f6a2871d05b03ffbe5ce4d2aa6be5276
Michael Kowollik <Michael.Kowollik@smail.inf.h-brs.de>
2020-08-09 17:41:41 +0200

    Added file extension filter to directory

--- src/main/java/Control/Camunda/UseCaseDelegate.java
+++ src/main/java/Control/Camunda/UseCaseDelegate.java
@@ -4,11 +4,15 @@ import Control.Parser.Parser;
 import Control.Strategy.UseCaseStrategy.Control.UseCaseStrategy;
 import Model.UMLComponent;
 import com.opencsv.CSVWriter;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOCase;
+import org.apache.commons.io.filefilter.SuffixFileFilter;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
 import javax.inject.Named;
 import java.io.File;
+import java.io.FileFilter;
 import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,46 +25,40 @@ public class UseCaseDelegate implements JavaDelegate {
 
         String homeDir = System.getProperty("user.home");
         homeDir = homeDir.replace("\\", "\\\\");
-        File KorrekturDir =  new File(homeDir +"\\Documents\\Korrektur");
-        File[] filesList = KorrekturDir.listFiles();
+        File KorrekturDir = new File(homeDir + "\\Documents\\Korrektur");
+        KorrekturDir.getParentFile().mkdirs();
+        File[] filesList = KorrekturDir.listFiles((FileFilter) new SuffixFileFilter(".uxf", IOCase.INSENSITIVE));
 
-        double beta = ((Long) execution.getVariable("beta"))/(double)100;
+        double beta = ((Long) execution.getVariable("beta")) / (double) 100;
         boolean checksimilarity = (Boolean) execution.getVariable("checksimilarity");
         assert filesList != null;
         Parser a = new Parser();
         UseCaseStrategy strategy = new UseCaseStrategy(beta, filesList.length);
         if (checksimilarity) {
-            double alpha = ((Long) execution.getVariable("alpha"))/(double)100;
-            double delta = ((Long) execution.getVariable("delta"))/(double)100;
-            strategy = new UseCaseStrategy(beta,alpha,delta, filesList.length);
+            double alpha = ((Long) execution.getVariable("alpha")) / (double) 100;
+            double delta = ((Long) execution.getVariable("delta")) / (double) 100;
+            strategy = new UseCaseStrategy(beta, alpha, delta, filesList.length);
         }
-
         try {
-            File csvfile = new File(homeDir +"\\Documents\\Rückmeldung\\response.csv");
+            File csvfile = new File(KorrekturDir + "\\response.csv");
             csvfile.getParentFile().mkdirs();
             CSVWriter writer = new CSVWriter(new FileWriter(csvfile));
             for (File file : filesList) {
                 List<UMLComponent> list = a.parseFile(file);
                 strategy.analyzeUML(list);
 
-
                 List<String> csvlist = new ArrayList<String>();
                 csvlist.add(file.getName());
                 csvlist.add(strategy.getStatus());
                 String[] nextLine = csvlist.toArray(new String[0]);
-                System.out.println(Arrays.toString(nextLine));
                 writer.writeNext(nextLine);
             }
             writer.close();
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Something went wrong when writing the csv file");
         }
-
-
-
         execution.setVariable("Report", strategy.getReport());
     }
 }
-
 
 
